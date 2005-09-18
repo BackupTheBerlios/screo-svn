7d973cc7cb5dbabaf45248e9f5ff96dc11677a00
stefanorausch <stefanorausch@cbf5c8ae-a8fa-cd4d-9320-10daad9bf0d6>
2005-09-18 15:16:35 +0000

    + ext.dependencies.php   external dependencies documentation file

---
+++ trunk/ext.dependencies.php
@@ -0,0 +1,46 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * External dependencies documentation file
+ *
+ * @category   Tools
+ * @package    ScriptReorganizer
+ * @subpackage Dependencies
+ * @author     Stefano F. Rausch <stefano@rausch-e.net>
+ * @version    SVN: $Id$
+ * @link       http://pear.php.net/package/ScriptReorganizer
+ * @since      File available since Release 0.3.0
+ * @filesource
+ */
+
+return;
+
+/**
+ * PHP 5 error handling mechanism
+ *
+ * @package    Dependencies
+ * @subpackage PEAR
+ * @link       http://pear.php.net/package/PEAR
+ */
+class PEAR_Exception {}
+
+/**
+ * Create and use PHP Archive files
+ *
+ * @package    Dependencies
+ * @subpackage PHP_Archive
+ * @link       http://pear.php.net/package/PHP_Archive
+ */
+class PHP_Archive_Creator {}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
+?>
